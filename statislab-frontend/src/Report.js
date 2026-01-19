d5153cbe360488b3a15cfe0a0bc17782b8d8de70
Freshtanazari <freshtanazari0m@gmail.com>
2026-01-19 12:04:28 +0500

    Added the analysis, report and analysisdata files

---
+++ statislab-frontend/src/Report.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Report = () =>{
+    return (
+        <div className="reportScreen">
+            <div className="reportContent">
+                Report Page
+            </div>
+        </div>
+    )
+}
+
+export default Report;
